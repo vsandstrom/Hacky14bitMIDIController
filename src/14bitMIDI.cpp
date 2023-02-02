91dd3a99fb1d3ea175813f09569647aefc95e595
Viktor Sandström <sandstrom.viktor@gmail.com>
2023-02-02 11:39:47 +0100

    values update but bleed over to neighbors. faulty timing with multiplexer most likely

--- src/14bitMIDI.cpp
+++ src/14bitMIDI.cpp
@@ -1,23 +1,29 @@
+#include "cd74.hpp"
+#include <unistd.h>
 #include "Arduino.h"
 #include <ResponsiveAnalogRead.h>
-//#include "usb_name.c"
 
 // number of faders to read from 
-#define COUNT 1
+#define COUNT 4
 // number of bits
-#define FOURTEEN 14
 // maximum value to normalize input tu
 #define RESOLUTION 16383
-// input reading point
-#define IN A0
 
 // hardcoded values to general purpose midi out, 16 (coarse), 48 (fine)
 #define MSB 16
 #define LSB 48
 
+#define ENABLE 9 
+#define SW1 10
+#define SW2 11
+#define SW3 12
+// input reading point
+#define IN 14
 
 // Responsive analog reader
 ResponsiveAnalogRead reader(IN, true);
+
+Multiplex mux = Multiplex(IN, 0, SW1, SW2, SW3, ENABLE);
 
 int faders[COUNT] = {0};
 
@@ -42,9 +48,10 @@ int removeBadValues(int);
 
 void setup() {
   // Set teensy to use 14bit and the analog reader to use 16383 as resolution.
-  analogReadResolution(FOURTEEN);
-  reader.setAnalogResolution(RESOLUTION);
-  Serial.begin(31250);
+    mux.initialize();
+    analogReadResolution(IN);
+    reader.setAnalogResolution(RESOLUTION);
+    Serial.begin(31250);
 
 }
 
@@ -52,35 +59,40 @@ void setup() {
 int i = 0;
 
 void loop() {
-  reader.update();
-  
-  // Sample 32 values for each fader
-    if (i < 32) {
-        for (int j = 0; j < COUNT; ++j) {
 
-        // Insert MULTIPLEX-logic here <--------------------------------------------------
-        
+    int j = 1;
+
+  reader.update();
+
+    if (i < 8) {
+        // for (int j = 0; j < COUNT; ++j) {
+
+            mux.gate(j);
+            delay(15);
             faders[j] += map(reader.getValue(), 1, 16382, 0, 16383);
-        }
+
+
+        //}
         i++;
+
     } else {
         i = 0;
-        for (int j = 0; j < COUNT; ++j) {
-
+        // for (int j = 0; j < COUNT; ++j) {
 
         // Shift value by 5, "dividing by 32"
         // remove bad values, if negative values or values bigger than 16383
-        faders[j] = removeBadValues(faders[j] >> 5);
+        faders[j] = removeBadValues(faders[j] >> 3);
 
         // send CC to correct midi destination, sequencial LSB and MSB.
         usbMIDI.sendControlChange(LSB + j, faders[j] & 127, 1);
         usbMIDI.sendControlChange(MSB + j, (faders[j] >> 7) & 127, 1);
 
         // Will only run if -DDEBUG flag is used at compile time
-        D(Serial.printf("VAL: %i\tLSB: %i\tMSB: %i\n", faders[j], (faders[j] & 0x7F), ((faders[j] >> 7) & 0x7F)));
+        // D(Serial.printf("FADER%i: %i\tLSB: %i\tMSB: %i\n", j+1, faders[j], (faders[j] & 0x7F), ((faders[j] >> 7) & 0x7F)));
+        D(Serial.printf("| %i\t | %i\t | %i\t | %i\t |\n", faders[0], faders[1], faders[2], faders[3]));
 
-        } 
-    delay(1);
+
+        //} 
 
     }
 }
